6e2da9d3d17c9463b2a1376c6a263936c0e8afe9
eleveyuan <eleveyuan@gmail.com>
2022-01-29 11:00:01 +0800

    commit

---
+++ lc28/strstr.cpp
@@ -0,0 +1,14 @@
+#include "strstr.h"
+
+
+int Solution::strStr(string haystack, string needle) {
+    int par, chi;
+    
+    for (int i = 0; i < haystack.size(); i ++) {
+        for(int j = 0; j < needle.size(); j ++) {
+
+        }
+    }
+
+    return 0;
+}
---
+++ lc28/strstr.h
@@ -0,0 +1,14 @@
+#ifndef __strstr_h
+#define __strstr_h
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    int strStr(string haystack, string needle); // 实现strstr函数
+};
+
+#endif 
